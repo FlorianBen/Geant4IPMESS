9e0857481b1f0d49d7c5a963ddf11415fc3e5389
Florian Benedetti <florian.benedetti@cea.fr>
2018-12-21 13:15:59 +0100

    je modifie le main

--- main.cpp
+++ main.cpp
@@ -4,34 +4,33 @@
 #include "G4RunManager.hh"
 #endif
 
-#include "G4UImanager.hh"
-#include "G4PhysListFactory.hh"
-#include <FTFP_BERT.hh>
 #include "G4GDMLParser.hh"
 #include "G4GenericBiasingPhysics.hh"
+#include "G4PhysListFactory.hh"
+#include "G4UImanager.hh"
+#include <FTFP_BERT.hh>
 
-#include "G4VisExecutive.hh"
 #include "G4UIExecutive.hh"
+#include "G4VisExecutive.hh"
 
-#include "ESSDetectorConstruction.hh"
 #include "ESSActionInitialization.hh"
+#include "ESSDetectorConstruction.hh"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   // Detect interactive mode (if no arguments) and define UI session
   //
-  G4UIExecutive* ui = 0;
-  if ( argc == 1 ) {
+  G4UIExecutive *ui = 0;
+  if (argc == 1) {
     ui = new G4UIExecutive(argc, argv);
   }
-  
+
   // Construct the run manager
   //
 #ifdef G4MULTITHREADED
-  G4MTRunManager* runManager = new G4MTRunManager;
-  runManager->SetNumberOfThreads(2);
+  G4MTRunManager *runManager = new G4MTRunManager;
+  runManager->SetNumberOfThreads(G4Threading::G4GetNumberOfCores());
 #else
-  G4RunManager* runManager = new G4RunManager;
+  G4RunManager *runManager = new G4RunManager;
 #endif
 
   runManager->SetUserInitialization(new ESSDetectorConstruction());
@@ -41,37 +40,34 @@ int main(int argc, char *argv[])
   physicsList->SetVerboseLevel(1);
   runManager->SetUserInitialization(physicsList);
 
-    // User action initialization
+  // User action initialization
   runManager->SetUserInitialization(new ESSActionInitialization());
-
 
   // Initialize visualization
   G4VisManager *visManager = new G4VisExecutive;
   visManager->Initialize();
 
   // Get the pointer to the User Interface manager
-  G4UImanager* UImanager = G4UImanager::GetUIpointer();
+  G4UImanager *UImanager = G4UImanager::GetUIpointer();
 
-  if ( ! ui ) { 
+  if (!ui) {
     // batch mode
     G4String command = "/control/execute ";
     G4String fileName = argv[1];
-    UImanager->ApplyCommand(command+fileName);
-  }
-  else {
+    UImanager->ApplyCommand(command + fileName);
+  } else {
     // interactive mode : define UI session
-    UImanager->ApplyCommand("/control/execute init_vis.mac"); 
+    UImanager->ApplyCommand("/control/execute init_vis.mac");
     if (ui->IsGUI()) {
-       UImanager->ApplyCommand("/control/execute icons.mac");
+      UImanager->ApplyCommand("/control/execute icons.mac");
     }
     ui->SessionStart();
     delete ui;
   }
 
-
   delete visManager;
   delete runManager;
   return 0;
-  
+
   return 0;
 }
