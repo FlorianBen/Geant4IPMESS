b0f661971b6a81d69b53f630667ea987a45020e8
Florian Benedetti <florian.benedetti@cea.fr>
2019-01-14 23:27:49 +0100

    update macro for camera

--- camera.C
+++ camera.C
@@ -1,7 +1,7 @@
 void camera(){
   auto canv = new TCanvas("canv1", "Camera", 1920, 1280);
   canv->Divide(2,1);
-  TFile *f = new TFile("camera.root", "read");
+  TFile *f = new TFile("camera_hist.root", "read");
   TTree *t1 = (TTree *)f->Get("Camera0");
   TTree *t2 = (TTree *)f->Get("Camera1");
   canv->cd(1);
